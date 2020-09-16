8fb7d426739949cb31a35705b51622a87da1d096
SimoneGIT <tarikul15-7944@diu.edu.bd>
2020-09-16 18:01:05 +0600

    Complite Passport

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,14 +11,24 @@ class LoginController extends Controller
 {
     public function login(Request $request){
 
-        $login = $request->validate([
+
+        $request->validate([
             'email'=> 'required',
             'password'=>'required'
         ]);
 
-      /*  if(Auth::attempt($login)){
+        $login = request(['email','password']);
 
-        }*/
+        if( !Auth::attempt( $login )){
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+        $user = User::find(1);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response(['user'=> $user,'authToken'=>$accessToken]);
 
     }
 }
